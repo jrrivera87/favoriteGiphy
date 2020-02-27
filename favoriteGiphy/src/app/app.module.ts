c8d3557e5496bd88c3eb98507415a1be3f8534fb
Baldemar Rivera <jrrivera87@gmail.com>
2020-02-27 07:35:05 -0600

    Initial commit

--- favoriteGiphy/src/app/app.module.ts
+++ favoriteGiphy/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiphyDisplayComponent } from './component/giphy-display/giphy-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GiphyDisplayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
